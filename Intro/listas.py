442a87ad27dbb36a3fb8d55275861609ee9ba272
bardo <bar2blues@gmail.com>
2025-03-07 19:29:54 -0300

    concepto de listas

---
+++ Intro/listas.py
@@ -0,0 +1,15 @@
+lenguajes=["php", "js", "node", "java"]
+
+print("lenguaje: " + lenguajes[1])
+
+lenguajes[1] = "python"
+
+print(lenguajes)
+
+lenguajes.append(("js"))
+
+print(lenguajes)
+
+lenguajes.remove(("java"))
+
+print(lenguajes)
