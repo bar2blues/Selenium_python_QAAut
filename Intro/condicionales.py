404acd476fa55f6942a68a3f9c12d59a3f04b0e3
bardo <bar2blues@gmail.com>
2025-03-12 19:15:49 -0300

    intro a condicionales

---
+++ Intro/condicionales.py
@@ -0,0 +1,12 @@
+a = 20
+b = 10
+
+# if (a > b):
+#     print("El mayor es A: " + str(a))
+# else:
+#     print("el mayor es B: " + str(b))
+
+if (a<=b):
+    print("{} es menor o igual que {}". format(a,b))
+else:
+    print ("{} es mayor o igual que {}". format(a,b))
