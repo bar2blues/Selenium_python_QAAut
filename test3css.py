e2fe5294703bd454cc0382e501d875085c244a7e
bardo <bar2blues@gmail.com>
2025-05-25 20:09:35 -0300

    find elements

---
+++ test3css.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver=webdriver.Chrome()
+driver.get("https://demoqa.com/text-box")
+driver.maximize_window()
+time.sleep(2)
+
+nom = driver.find_element(By.CSS_SELECTOR, "#userName")
+nom.send_keys("Fede")
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "#userEmail").send_keys("fede@mail.com")
+time.sleep(2)
+driver.execute_script("window.scrollTo(0, 500)")
+time.sleep(2)
+button = driver.find_element(By.CSS_SELECTOR, "#submit")
+button.click()
+time.sleep(2)
+
+
+driver.close()
